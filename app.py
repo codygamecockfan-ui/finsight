f7f210ff00909fae4620800fb2bab66b580f3150
codygamecockfan-ui <codygamecockfan@gmail.com>
2026-03-12 14:56:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 app = Flask(**name**)
 app.secret_key = os.getenv(“FLASK_SECRET_KEY”, os.urandom(24))
-client = Anthropic()
+client = Anthropic() 
 
 ANTHROPIC_API_KEY  = os.getenv(“ANTHROPIC_API_KEY”)
 POLYGON_API_KEY    = os.getenv(“POLYGON_API_KEY”)
