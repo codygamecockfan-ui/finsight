05a0333e190fd7bcb99b65d9829a1d39285691c4
codygamecockfan-ui <codygamecockfan@gmail.com>
2026-03-18 12:44:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1189,4 +1189,4 @@ if __name__ == "__main__":
 port = int(os.environ.get("PORT", 5000))
 debug = os.environ.get("FLASK_ENV") != "production"
 print(f" FinSight is running at http://localhost:{port}")
-app.run(debug=debug, host="0.0.0.0", port=port)
+app.run(debug=debug, host="0.0.0.0", port=port) 
